75f7aa4077cdbf4462ee1d5d3d4641e031f178ed
boris <borislav.sabotinov@gmail.com>
2019-09-02 15:42:46 -0500

    Switch to JUnit5 (JUnit Platform + JUnit Jupiter + JUnit Vintage)

--- test/datastructures/BasicQueueTest.java
+++ test/datastructures/BasicQueueTest.java
@@ -1,20 +1,21 @@
 package datastructures;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
  * Generic type arguments must be reference types.
  * Since primitives do not extend Object they cannot be used as generic type arguments for a parametrized type.
  */
-public class BasicQueueTest {
+class BasicQueueTest {
     private BasicQueue<Integer> myQueue;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         myQueue = new BasicQueue<>(5);
         myQueue.enQueue(5);
         myQueue.enQueue(6);
@@ -22,7 +23,7 @@ public class BasicQueueTest {
     }
 
     @Test
-    public void testEmptySize() {
+    void testEmptySize() {
         while (myQueue.size() > 0) {
             myQueue.deQueue();
         }
@@ -30,30 +31,30 @@ public class BasicQueueTest {
     }
 
     @Test
-    public void testEnqueueAndSize() {
+    void testEnqueueAndSize() {
 
         assertEquals(3, myQueue.size());
     }
 
     @Test
-    public void testDequeue() {
+    void testDequeue() {
         int x = myQueue.deQueue();
         assertEquals(5, x);
     }
 
     @Test
-    public void testContains() {
+    void testContains() {
         assertTrue(myQueue.contains(10));
     }
 
     @Test
-    public void testAccess() {
+    void testAccess() {
         int x = myQueue.access(1);
         assertEquals(6, x);
     }
 
     @Test
-    public void testToString() {
+    void testToString() {
         assertEquals("[5, 6, 10]", myQueue.toString());
     }
 }
