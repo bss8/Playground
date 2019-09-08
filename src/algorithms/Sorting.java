afe41c10a7c4ae75387c47d8cb1d063e93378a97
boris <borislav.sabotinov@gmail.com>
2019-09-08 16:37:32 -0500

    Adding insertion sort implementation

--- src/algorithms/Sorting.java
+++ src/algorithms/Sorting.java
@@ -1,15 +1,32 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
- *
  * @param <T>
  */
-public class Sorting<T> implements SortingAlgorithms<T> {
-    public void bubbleSort(T dataObj){
+public class Sorting<T extends Comparable<? super T>> implements SortingAlgorithms<T> {
+    public void bubbleSort(T dataObj) {
 
     }
 
-    public void insertionSort(T dataObj) {
+    public void insertionSort(ArrayList<T> objList) {
+
+        for (int i = 1; i < objList.size(); i++) {
+            T key = objList.get(i);
+            int j = i - 1;
+
+            while ((j >= 0) && objList.get(j).compareTo(key) > 0) {
+                objList.set(j + 1, objList.get(j));
+                j = j - 1;
+            }
+            objList.set(j + 1, key);
+        }
+    }
+
+    @Override
+    public void selectionSort(T dataObj) {
 
     }
 
@@ -27,6 +44,27 @@ public class Sorting<T> implements SortingAlgorithms<T> {
 
     public void radixSort(T dataObj) {
 
+    }
+
+    private int generateRandomInt() {
+        Random r = new Random(System.nanoTime());
+        return r.nextInt(100) + 1;
+    }
+
+    public static void main(String...args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        Sorting sorting = new Sorting();
+
+        for (int i = 0; i < 11; i++) {
+            int x = sorting.generateRandomInt();
+            intList.add(x);
+        }
+
+        System.out.println("Unsorted list: " + intList.toString());
+
+        sorting.insertionSort(intList);
+
+        System.out.println("Sorted list: " + intList.toString());
     }
 
 }
--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -1,8 +1,11 @@
 package algorithms;
 
+import java.util.ArrayList;
+
 public interface SortingAlgorithms<T> {
     void bubbleSort(T dataObj);
-    void insertionSort(T dataObj);
+    void insertionSort(ArrayList<T> objList);
+    void selectionSort(T dataObj);
     void mergeSort(T dataObj);
     void quickSort(T dataObj);
     void heapSort(T dataObj);
